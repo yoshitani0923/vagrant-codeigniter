68f600b4aa5db1c84fab01e30836f19e389d6089
yyoshitani <yoshitani@realworld.jp>
2014-06-18 18:21:22 +0900

    date change

--- sendmail.sh
+++ sendmail.sh
@@ -6,10 +6,12 @@ address="yoshitani@realworld.jp"
 #メール主題
 #`date`は現在の日時を表示
 subject="`date`"
-
+date=$(LANG=E date -d '1 days ago' '+%d/%b')
 #アクセスログから上位３件取得
-sudo cat /var/log/httpd/vagrant-codeigniter-access_log | awk -F ' ' '{print $7}' | sort | uniq -c > /vagrant/count.txt
-message=$(sudo cat /vagrant/count.txt | awk -F ' ' '{ print $0 }' | sort -k 1 -n -r | head -n 3)
+sudo cat /var/log/httpd/vagrant-codeigniter-access_log | awk -F ' ' '{print $7 " " $4}' | sort | uniq -c > /vagrant/count.txt
+sudo cat /vagrant/count.txt | awk -F ' ' '{ print $0 }' | sort -k 1 -n -r | head -n 3 > /vagrant/sort.txt
+grep $date < /vagrant/sort.txt > message.txt
+message=$(cat /vagrant/message.txt | awk -F ' ' '{print "アクセス回数："$1 " アクセス先：" $2}')
 
 #メール送信
 mail -s "$subject" $address << mailbody
